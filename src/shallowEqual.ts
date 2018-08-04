c825b926b403cea7f4aec3ab9fade4b5aa15e667
黄斌杰 <akira.binjie@gmail.com>
2018-08-04 18:41:15 +0800

    修复了一个浅比较不必对象属性长度的问题

--- src/shallowEqual.ts
+++ src/shallowEqual.ts
@@ -21,7 +21,7 @@ export default function shallowEqual<O extends { [key: string]: any }>(objA: O, 
   const keysA = Object.keys(objA)
   const keysB = Object.keys(objB)
 
-  // if (keysA.length !== keysB.length) return false
+  if (keysA.length !== keysB.length) return false
 
   for (let i = 0; i < keysA.length; i++) {
     if (!hasOwn.call(objB, keysA[i]) ||
